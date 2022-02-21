3897668b307591c72b3a178652116953a05cd2d0
Evil_Hacker437 <86729101+DJDarkCyber@users.noreply.github.com>
2022-02-21 22:57:41 +0530

    Add files via upload

---
+++ movieVirus_v1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+
+import subprocess
+import time
+import pymsgbox
+
+def messageBox():
+    smp1 = ["Ok, Go it", "Fuck Back!"]
+    usrRep = pymsgbox.confirm("You computer Got Hacked By Dr.Hecker!", "Fucked Up!", smp1)
+    if usrRep == smp1[0]:
+        pymsgbox.alert("You are kind about this system!")
+        return "1"
+    elif usrRep == smp1[1]:
+        pymsgbox.alert("Your computer Going to get Fucked up again!")
+        return "2"
+    pymsgbox.alert("You System Got Fucked!")
+
+def myLittleBot():
+    subprocess.call("xterm -fullscreen -e /bin/bash -l -c 'hollywood'", shell=True)
+someday = messageBox()
+if someday == "2":
+    myLittleBot()
